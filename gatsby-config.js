e793f885c31ec8154e5070cd59ff2d2957a46863
Daniel Olson <emaildano@gmail.com>
2020-01-09 20:22:55 -0500

    VC edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,11 +62,8 @@ module.exports = {
     {
       resolve: `gatsby-source-graphql`,
       options: {
-        // This type will contain remote schema Query type
         typeName: `WPGraphQL`,
-        // This is field under which it's accessible
         fieldName: `wpgraphql`,
-        // Url to query from
         url: `https://demo.wpgraphql.com/graphql`,
       },
     },
