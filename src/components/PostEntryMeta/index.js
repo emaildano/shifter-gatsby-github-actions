f28806c6e94729ebf974c1e59b205dba124e8fd9
Daniel Olson <emaildano@gmail.com>
2020-01-09 20:15:51 -0500

    removed author meta

--- src/components/PostEntryMeta/index.js
+++ src/components/PostEntryMeta/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import moment from "moment/moment"
-import { Row, Col, Avatar } from "antd"
-import { Link } from "gatsby"
+import { Row, Col } from "antd"
+// import { Link } from "gatsby"
 
 const PostEntryMeta = ({ post }) => (
   <Row
@@ -10,12 +10,14 @@ const PostEntryMeta = ({ post }) => (
     gutter={16}
   >
     <Col xs={24} md={24} style={{ textAlign: `center` }}>
-      <Link to={`/author/${post.author.slug}`}>
-        <Avatar size={100} src={post.author.avatar.url} />
-      </Link>
+      {/* <Link to={`/author/${post.author.slug ? post.author.slug : '#'}`}> */}
+        {/* <Avatar size={100} src={post.author.avatar.url} /> */}
+      {/* </Link> */}
     </Col>
     <Col xs={24} md={24} style={{ textAlign: `center` }}>
-      <Link to={`/author/${post.author.slug}`}>{post.author.name}</Link>
+      {/* <Link to={`/author/${post.author.slug ? post.author.slug : '#'}`}> */}
+        {/* {post.author.name} */}
+      {/* </Link> */}
       <br />
       {moment(post.date).format(`MMM Do YY`)}
     </Col>
